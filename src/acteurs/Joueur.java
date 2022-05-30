d181ba3bef5d3399cf4a4d3acc794bf10c835cf2
albac <clement.alba2.etu@univ-lille.fr>
2022-05-30 17:59:25 +0200

    CONFIG

--- src/acteurs/Joueur.java
+++ src/acteurs/Joueur.java
@@ -42,7 +42,7 @@ public abstract class Joueur extends Acteur implements StyleJoueur{
         ETAT.getInvestissements().add(investissement);
     }
 
-    public void payerBFP(){
+    public void payerBFP(boolean foncier, float taxe){
 
     }
 
