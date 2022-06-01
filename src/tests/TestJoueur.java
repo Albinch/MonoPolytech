681b089b5a32b0b99c749e7170f2eb7a948e9a13
albac <clement.alba2.etu@univ-lille.fr>
2022-06-01 17:21:03 +0200

    fix joueur prudent

---
+++ src/tests/TestJoueur.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestJoueur {
+    @Test
+    public void testbla(){
+        int a = 5;
+        Assert.assertEquals(2, a, 0);
+    }
+}
